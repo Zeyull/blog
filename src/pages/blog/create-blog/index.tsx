d39b728f82766bb0fd56a21efa01d2e011e8c780
nozomi <13508085664@163.com>
2024-07-25 00:25:56 +0800

    [other]record

--- src/pages/blog/create-blog/index.tsx
+++ src/pages/blog/create-blog/index.tsx
@@ -1,17 +1,36 @@
 import { ArrowLeftOutlined } from '@ant-design/icons';
-import { history } from 'umi';
+import { Button, Input } from 'antd';
+import { useState } from 'react';
+import { FormattedMessage, history, useIntl } from 'umi';
 import styles from './index.less';
 
 export default function CreateBlogPage() {
+  const intl = useIntl();
+  const [title, setTitle] = useState('');
+
+  const titlePlaceholder = intl.formatMessage({
+    id: 'blog_create_title_input_placeholder',
+  });
   const backPage = () => {
     history.go(-1);
   };
-
+  // todo 看看之前是怎么写的
   return (
     <div className={styles.content}>
       <header>
         <ArrowLeftOutlined onClick={backPage} />
+        <Input
+          placeholder={titlePlaceholder}
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <Button type="primary">
+          <FormattedMessage id="blog_create_button_label" />
+        </Button>
       </header>
+      {title}
     </div>
   );
 }
